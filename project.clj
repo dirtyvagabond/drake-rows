cbdc9eef1851c182153cf6c2bd7c529474cf2453
Aaron Crow <dirtyvagabond@yahoo.com>
2015-06-26 11:29:29 -0700

    misc project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject dirtyvagabond/drake-rows "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject drake-rows "0.1.0-SNAPSHOT"
+  :description "Plug Rows into Drake"
+  :url "https://github.com/dirtyvagabond/drake-rows"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
